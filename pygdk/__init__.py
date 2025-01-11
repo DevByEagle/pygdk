6a327dc1664a5f7cd29ae9c4bc83a8c0d34faeaa
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-01-11 15:00:14 -0500

    [DEV]: Updated pygdk ui

--- pygdk/__init__.py
+++ pygdk/__init__.py
@@ -6,4 +6,10 @@ window = ctk.CTk()
 def init(width, height, title):
     window.title(title)
     window.geometry(f"{width}x{height}")
+
+def drawText(text, font=("Arial", 12), color="black"):
+    label = ctk.CTkLabel(window, text=text, font=font, text_color=color)
+    label.pack()
+
+def show():
     window.mainloop()
--- pygdk/__init__.pyi
+++ pygdk/__init__.pyi
@@ -5,7 +5,9 @@ from typing import NoReturn
 # Window and Graphics Device Functions
 #======================================
 def init(width: int, height: int, title: str) -> NoReturn: ...
+def show() -> None: ...
 
 #======================================
 # Font and Text Functions
 #======================================
+def drawText(text: str, font=("Arial", 12), color="black") -> None: ...
